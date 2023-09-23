43f70c27473eead59b99071c58fdc579c2e55498
Tom Keene <tom@theanthillsocial.co.uk>
2023-09-23 23:58:14 +0100

    Added p5 template

---
+++ template-p5js/sketch.js
@@ -0,0 +1,14 @@
+//THIS IS A COMMENT!!! A template sketch for p5.js
+
+//run once when our index.html file is first loaded
+function setup() {
+  createCanvas(500, 500); //width in pixels, height in pixels
+
+  //draw a black background
+  background(0);
+  //set fill colour to white
+  fill(255);
+  //draw an ellipse
+  ellipse(250, 250, 200, 200);
+}
+
