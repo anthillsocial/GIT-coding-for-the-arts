a02b4260e3ac86c09355e21d81e8f5989250195e
Tom Keene <tom@theanthillsocial.co.uk>
2023-09-28 15:21:18 +0100

    Reworked template

--- week02-color+form/boxes/sketch.js
+++ week02-color+form/boxes/sketch.js
@@ -1,36 +1,30 @@
-/* * * * * * * * * * * * * * * * * * * * * * * *
-          MA1805 Grey Squares
- * * * * * * * * * * * * * * * * * * * * * * * */
-
-
 //setup is run once when the webpage is first loaded
 function setup() {
-  createCanvas(500, 500); //this is a 500 pixel by 500 pixel square canvas
-  
-  //turn off outlines
-  noStroke();
-  
-    //set the background colour to 255 (white)
-  background(255);
-  
-  //set fill color to black
-  fill(0);
-  //draw a 100-pixel square in the top left of the canvas
-  rect(0, 0, 100, 100);
-  
-  //fill is dark grey
-  fill(64);
-  //another square 100 pixels in from the top left
-  rect(100, 100, 100, 100);
-  
-  //fill is medium grey
-  fill(128)
-  //again, this time 200 pixels in on the x and y axes
-  rect(200, 200, 100, 100);
-  
-  //fill is light grey
-  fill(196);
-  //draw a 200-pixel square, 300 pixels in on the x and y axes
-  rect(300, 300, 200, 200);
-  
+  createCanvas(500, 500); // 500x500 pixel canvas
+  background(255);        // White background 
+  noStroke();             // no outlines on shapes 
 }
+
+// Runs on an infinite loop (around 60fps)
+function draw(){
+    // Black | rectangle 100px wide, 100px high | 0px from left | 0px from top 
+    fill(0); // Define the fill color
+    rect(0, 0, 100, 100); // x & y coordinates | width & height 
+         
+    // Dark grey | rect h=100 w=100px | 50px from left | 60px from top   
+    fill(64);
+    rect(50, 50, 100, 100);
+    
+    // Mid grey | 100x100px rect | x=50px  | y=60px  
+    fill(128)
+    rect(100, 100, 100, 100);
+    
+    // Light grey | 100x100 px rect | 50px  | 60px from top 
+    fill(196);
+    rect(150, 150, 200, 200);
+
+    // Light grey | 100x100 px rect | 50px  | 60px from top 
+    fill(220);
+    rect(300, 300, 400, 400);
+}
+  
