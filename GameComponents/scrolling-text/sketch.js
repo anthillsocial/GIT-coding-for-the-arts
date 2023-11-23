c42720e6efd42833d62a3fba2c7e0f3e028f3cc5
Tom Keene <tom@theanthillsocial.co.uk>
2023-11-23 11:13:20 +0000

    Added scrolling text example

---
+++ GameComponents/scrolling-text/sketch.js
@@ -0,0 +1,43 @@
+let lines
+let linesY = []; 
+let scrollSpeed = 0.8; // Speed of scroll
+let lineHeight = 15;   // Space between lines 
+
+function preload() {
+  lines = loadStrings('sketch.js');
+}
+
+function setup() {
+  createCanvas(400, 400);
+  setupText()
+}
+
+function draw() {
+  background(150);
+  fill(200, 0, 0); 
+  textScroll(); 
+}
+
+// Scroll the text
+function textScroll(){
+  push(); // Save colours/tranforms from the main draw
+  fill(0); 
+  for (let i = 0; i < lines.length; i++) {
+    text(lines[i], 100, linesY[i]); // text, x, y
+    linesY[i] -= scrollSpeed; 
+  }
+  // Check of the last line has disappeared off the top
+  if(linesY[lines.length-1] <= 0){
+    setupText(); 
+  }
+  pop(); // Back to original colours/transforms
+}
+
+// Generate the starting Y positions
+function setupText(){
+  // Loop through each line of text
+  for (let i = 0; i < lines.length; i++) {
+    // Define a starting position for a line 
+    linesY[i] = i*lineHeight+height; 
+  }
+}
